b86f06fc935939d4c9830ecedf0fa0cce4fe345b
Ghufran Syed <ghufran.syed@gmail.com>
2020-05-19 10:23:38 -0700

    Initial commit

---
+++ src/com/ghufran/core.cljs
@@ -0,0 +1,18 @@
+(ns com.ghufran.core
+  (:require [reagent.core :as r]
+            [reagent.dom :as rdom]))
+
+(defn simple-component []
+  [:div
+   [:p "I am a component"]
+   [:p.someclass
+    "I have " [:strong "bold"]
+    [:span {:style {:color "red"}} " and red "] "text."]])
+
+(defn ^:export run []
+  (rdom/render [simple-component] (js/document.getElementById "app")))
+
+
+(defn main []
+  (.log js/console "running main!")
+  (run))
