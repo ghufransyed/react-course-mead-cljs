f76d3e394f2ae24faf0c9f4b44fd269c26d70e91
Ghufran Syed <ghufran.syed@gmail.com>
2020-05-20 20:07:28 -0700

    update .gitignore

--- public/scripts/cljs-runtime/shadow.module.core.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('core');
-
-try { com.ghufran.core.run(); } catch (e) { console.error("An error occurred when calling (com.ghufran.core/run)"); throw(e); }
