19a4d46a8c58c92c37a99151a60442a0744ee7c8
Ghufran Syed <ghufran.syed@gmail.com>
2020-05-20 17:48:18 -0700

    make reagent components

--- public/scripts/cljs-runtime/com.ghufran.core.js
+++ public/scripts/cljs-runtime/com.ghufran.core.js
@@ -2,11 +2,26 @@ goog.provide('com.ghufran.core');
 goog.require('cljs.core');
 goog.require('reagent.core');
 goog.require('reagent.dom');
-com.ghufran.core.simple_component = (function com$ghufran$core$simple_component(){
-return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"p","p",151049309),"I am a component"], null),new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"p.someclass","p.someclass",-1904646929),"I have ",new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"strong","strong",269529000),"bold"], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"span","span",1394872991),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"style","style",-496642736),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"color","color",1011675173),"red"], null)], null)," and red "], null),"text."], null)], null);
+com.ghufran.core.header = (function com$ghufran$core$header(){
+return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h1","h1",-1896887462),"Indecision"], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h2","h2",-372662728),"Put your life in the hands of a computer"], null)], null);
+});
+com.ghufran.core.action = (function com$ghufran$core$action(){
+return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"button","button",1456579943),"What should I do?"], null)], null);
+});
+com.ghufran.core.option = (function com$ghufran$core$option(){
+return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"p","p",151049309),"This is the option component"], null)], null);
+});
+com.ghufran.core.options = (function com$ghufran$core$options(){
+return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"p","p",151049309),"This is the options component"], null),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [com.ghufran.core.option], null)], null);
+});
+com.ghufran.core.add_option = (function com$ghufran$core$add_option(){
+return new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"p","p",151049309),"This is the add-option component"], null)], null);
+});
+com.ghufran.core.indecision_app = (function com$ghufran$core$indecision_app(){
+return new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [com.ghufran.core.header], null),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [com.ghufran.core.action], null),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [com.ghufran.core.options], null),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [com.ghufran.core.add_option], null)], null);
 });
 com.ghufran.core.run = (function com$ghufran$core$run(){
-return reagent.dom.render.cljs$core$IFn$_invoke$arity$2(new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [com.ghufran.core.simple_component], null),document.getElementById("app"));
+return reagent.dom.render.cljs$core$IFn$_invoke$arity$2(new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [com.ghufran.core.indecision_app], null),document.getElementById("app"));
 });
 goog.exportSymbol('com.ghufran.core.run', com.ghufran.core.run);
 
--- src/com/ghufran/core.cljs
+++ src/com/ghufran/core.cljs
@@ -2,12 +2,48 @@
   (:require [reagent.core :as r]
             [reagent.dom :as rdom]))
 
-(defn simple-component []
+#_(defn simple-component []
   [:div
    [:p "I am a component"]
    [:p.someclass
     "I have " [:strong "bold"]
     [:span {:style {:color "red"}} " and red "] "text."]])
 
+
+
+
+(defn header []
+      [:div
+       [:h1 "Indecision"]
+       [:h2 "Put your life in the hands of a computer"]])
+
+
+(defn action []
+      [:div
+       [:button "What should I do?"]])
+
+(defn option []
+      [:div
+       [:p "This is the option component"]])
+
+
+(defn options []
+      [:div
+       [:p "This is the options component"]
+       [option]])
+
+
+(defn add-option []
+      [:div
+       [:p "This is the add-option component"]])
+
+
+(defn indecision-app []
+      [:div
+       [header]
+       [action]
+       [options]
+       [add-option]])
+
 (defn ^:export run []
-  (rdom/render [simple-component] (js/document.getElementById "app")))
+  (rdom/render [indecision-app] (js/document.getElementById "app")))
