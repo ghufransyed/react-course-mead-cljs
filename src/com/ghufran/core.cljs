510ddf5c6f6a8a616926e7d785abb080d92c039e
Ghufran Syed <ghufran.syed@gmail.com>
2020-06-07 20:36:58 -0700

    change state management,add transit

--- src/com/ghufran/core.cljs
+++ src/com/ghufran/core.cljs
@@ -15,9 +15,12 @@
     [:h2 subtitle]]))
 
 
+
 (defn pick_option [app_db]
   (let [picked_option (rand-nth (keys (:options @app_db)))]
-    (swap! app_db assoc-in [:selectedOption] picked_option)))
+    (do
+      (swap! app_db assoc-in [:selectedOption] picked_option))))
+
 
 
 (defn action [app_db]
@@ -31,43 +34,60 @@
 
 
 (defn option [{:keys [option_text app_db id]}]
-  [:div {:id (str "option-" id)
-         :class "option" }
-   [:span option_text
-    [:button {
-              :id    (str "btn-" id)
-              :class "option-remove-btn"
-              :on-click
-                     #(swap! app_db (fn [m] update-in m [:options] dissoc option_text))}
-     "Remove"]]])
+  (do
+    [:div {:id    (str "option-" id)
+           :class "option"}
+     [:span option_text
+
+      [:button {
+                :id    (str "btn-" id)
+                :class "option-remove-btn"
+                :on-click
+                       (fn [e]
+                         (do
+                           (swap! app_db update-in [:options] dissoc (keyword option_text))
+                           (u/set-item! "app_db" @app_db)
+                           ))}
+       "Remove"]]]))
+
+
+
+;(def test_db (atom {:options        {:one "1" :two "2"}
+;                    :selectedOption false}))
+;@test_db
+;(def option_text "two")
+;(update-in @test_db [:options] dissoc (keyword option_text))
+;(swap! test_db update-in [:options] dissoc (keyword option_text))
+;@test_db
+;(u/get-item "app_db")
+;(u/remove-item! "app_db")
+
 
 
 (defn handle_remove_all [app_db]
-  (swap! app_db merge {:options []}))
+  (do
+    (u/remove-item! "app_db")
+    (swap! app_db merge {:options {}})))
+
 
 
 (defn options [app_db]
-      [:div
-       [:button {:id "remove-all"
-                 :on-click
-                     #(handle_remove_all app_db)
-                 :disabled (empty? (:options @app_db))}
-        "Remove All"]
-       (for [[val uuid id] (map conj (:options @app_db) (range))
-             :let [key (str uuid)]]
-         (do
-           (js/console.log [val key id])
-           ^{:key key}
-           [option {:id          id
-                    :option_text val
-                    :app_db      app_db}]))
+  [:div
+   [:button {:id       "remove-all"
+             :on-click
+                       #(handle_remove_all app_db)
+             :disabled (empty? (:options @app_db))}
+    "Remove All"]
+   (for [[val uuid id] (map conj (:options @app_db) (range))
+         :let [key (str uuid)]]
+     ^{:key key}
+     [option {:id          id
+              :option_text val
+              :app_db      app_db}])
 
-       (if (= (count (:options @app_db)) 0)
-         [:p {:id "get-started-msg"}
-          "Please add an option to get started!"])
-       ])
-
-
+   (if (= (count (:options @app_db)) 0)
+     [:p {:id "get-started-msg"}
+      "Please add an option to get started!"])])
 
 
 
@@ -79,53 +99,48 @@
         (= new_option "")
         (do
           (swap! add_option_state merge {:error "Sorry, that is not a valid entry"}))
-        (contains? (:options @app_db) new_option)
-          (do
-            (swap! add_option_state merge {:error (str "This item already exists")}))
+        (contains? (:options @app_db) (keyword new_option))
+        (do
+          (swap! add_option_state merge {:error (str "This item already exists")}))
 
         :else
         (do
-          (reset! add_option_state { :form_value ""
-                                     :error      false})
+          (reset! add_option_state {:form_value ""
+                                    :error      false})
           (swap! app_db assoc-in
-                 [:options new_option] (str (random-uuid)))
+                 [:options (keyword new_option)] (str (random-uuid)))
           (u/set-item! "app_db" @app_db))))))
 
 
 
-
-
-
-
-
-
 (defn add-option [app_db]
-  (let [add_option_state (r/atom {:error false
+  (let [add_option_state (r/atom {:error      false
                                   :form_value ""})]
     (fn [app_db]
       (let [error (:error @add_option_state)]
         (do
-            [:div
-             (if error
-               [:p {:id "error-msg-addOption"} error])
-             [:form {:id "form-addOption"
-                     :on-submit
-                         (fn [e]
-                           (handle_add_option {:event            e
-                                               :add_option_state add_option_state
-                                               :app_db     app_db}))}
-              [:input {:id          "input-addOption"
-                       :placeholder "Enter option text here"
-                       :name        :add_option_text
-                       :on-change
-                                    (fn [e]
-                                      (swap! add_option_state assoc
-                                             :form_value e.target.value))
-                       :value       (:form_value @add_option_state)
-                       }]
-              [:button {:type :submit
-                        :id   "button-addOption"}
-               "Add Option"]]])))))
+          [:div
+           (if error
+             [:p {:id "error-msg-addOption"} error])
+           [:form {:id "form-addOption"
+                   :on-submit
+                       (fn [e]
+                         (handle_add_option {:event            e
+                                             :add_option_state add_option_state
+                                             :app_db           app_db}))}
+            [:input {:id          "input-addOption"
+                     :placeholder "Enter option text here"
+                     :name        :add_option_text
+                     :on-change
+                                  (fn [e]
+                                    (swap! add_option_state assoc
+                                           :form_value e.target.value))
+                     :value       (:form_value @add_option_state)
+                     }]
+            [:button {:type :submit
+                      :id   "button-addOption"}
+             "Add Option"]]])))))
+
 
 
 (defn option-modal [app_db]
@@ -136,55 +151,33 @@
       :id             "option-modal"
       :onRequestClose (fn [e]
                         (swap! app_db assoc-in [:selectedOption] false))
-      :ariaHideApp    false }
+      :ariaHideApp    false}
 
      [:h3 "Selected Option"]
      (if selectedOption
-       [:p {:id selectedOption} selectedOption])
+       [:p {:id "picked-option"} selectedOption])
      [:button {:id       "closeModal"
                :on-click (fn [e]
-                           (swap! app_db assoc-in [:selectedOption] false))}"Okay"]
-     ]))
+                           (swap! app_db assoc-in [:selectedOption] false))} "Okay"]]))
+
 
 
 (defn indecision-app []
-  (let [app_db (r/atom  {:options {} :selectedOption false})]
-    (r/create-class
-      {:display-name "indecision-app"
+  (let [app_db (r/atom (or
+                         (u/get-item "app_db")
+                         {:options {} :selectedOption false}))]
+    (fn []
+      [:div
+       [header {:subtitle
+                "Put your life in the hands of a computer"}]
+       [action app_db]
+       [options app_db]
+       [add-option app_db]
+       [option-modal app_db]
+       ])))
 
-       :component-did-mount
-                     (fn [this]
-                       (do
-                         (println "component-did-mount")
-                         (tap> @app_db)
-                         (let [ls (u/get-item "app_db")]
-                           (if ls
-                             (reset! app_db ls)))
-                         (tap> @app_db)
-                         ))
-
-       :reagent-render
-                     (fn []
-                       [:div
-                        [header {:subtitle
-                                 "Put your life in the hands of a computer"}]
-                        [action app_db]
-                        [options app_db]
-                        [add-option app_db]
-                        [option-modal app_db]
-                        ])})))
 
 
 (defn ^:export ^:dev/after-load run []
   (rdom/render [indecision-app]
                (js/document.getElementById "app")))
-
-
-#_(u/set-item! 'app_db' {:options {:one "1" :two "2"} :selectedOption false})
-
-
-(u/get-item "app_db")
-(boolean (u/get-item "app_db"))
-(boolean (u/get-item "foo"))
-
-
--- src/com/ghufran/utils.cljs
+++ src/com/ghufran/utils.cljs
@@ -1,16 +1,35 @@
-(ns com.ghufran.utils)
+(ns com.ghufran.utils
+  (:require [cognitect.transit :as t]))
 
 (defn set-item!
   "Set `key' in browser's localStorage to `val`."
   [key val]
-  (.setItem (.-localStorage js/window) key (.stringify js/JSON (clj->js val))))
+  (let [w (t/writer :json-verbose)]
+    (as-> js/window v
+          (.-localStorage v)
+          (.setItem v key (t/write w val)))))
 
 (defn get-item
   "Returns value of `key' from browser's localStorage."
   [key]
-  (js->clj (.parse js/JSON (.getItem (.-localStorage js/window) key))))
+  (let [r (t/reader :json)]
+    (as-> js/window v
+          (.-localStorage v)
+          (.getItem v key)
+          (t/read r v))))
 
 (defn remove-item!
   "Remove the browser's localStorage value for the given `key`"
   [key]
   (.removeItem (.-localStorage js/window) key))
+
+;(def app_db {:options {:one "1" :two "2"} :selectedOption false})
+;(set-item! "app_db" {:options {:one "1" :two "2"} :selectedOption false})
+;(get-item "app_db")
+;
+;(js->clj (.parse js/JSON (.stringify js/JSON (clj->js app_db))) {:keywordize-keys true})
+;(js->clj (clj->js app_db) {:keywordize-keys true})
+;
+;(set-item! "app_db" app_db)
+;(get-item "app_db")
+
